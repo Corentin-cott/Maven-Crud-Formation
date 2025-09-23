21925bb907be8564dc260b62f54860819bdf448a
corentin.cott@gmail.com <corentin.cott@gmail.com>
2025-09-23 12:33:54 +0200

    Lancement avec Spring, ajout d'un premier controller

---
+++ src/main/java/org/formation/HelloWorldController.java
@@ -0,0 +1,20 @@
+package org.formation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/// L'annotation `@RestController` indique que la classe est un contrôleur
+/// qui écoute les requêtes web et retourne des données sous forme de réponse HTTP.
+/// Elle est une combinaison de `@Controller` et `@ResponseBody`.
+@RestController
+public class HelloWorldController {
+
+    /// L'annotation `@RequestMapping` est utilisée pour mapper une méthode de
+    /// contrôleur à une requête HTTP. Elle définit l'URL et la méthode
+    /// HTTP (GET, POST, PUT, DELETE, etc.) que la méthode de controller peut gérer.
+    @RequestMapping("/hello")
+    public String helloworld() {
+        return "Hello World";
+    }
+
+}
