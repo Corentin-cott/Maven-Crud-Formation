3a13a9ddafe9e1899ac2d185842bfc4367358b10
corentin.cott@gmail.com <corentin.cott@gmail.com>
2025-09-24 10:37:45 +0200

    Ajout de commentaires sur les méthodes d'endpoints

--- src/main/java/org/formation/controllers/ClientController.java
+++ src/main/java/org/formation/controllers/ClientController.java
@@ -11,21 +11,37 @@ public class ClientController {
     @Autowired
     ClientRepository repository;
 
+    /// **Method GET**
+    ///
+    /// Méthode classique pour récupérer une ressource.
+    ///
+    /// `@PathVariable` précise que pour récupérer la variable `long id`, il faut prendre depuis la variable présente entre crochets dans l'URI : `{id}`.
     @RequestMapping(value="/client/{id}", method=RequestMethod.GET)
     public Client get(@PathVariable long id) {
         return repository.findById(id).orElse(null);
     }
 
-    @RequestMapping(value="/client/{id}", method=RequestMethod.POST)
+    /// **Method POST**
+    ///
+    /// Méthode spécialisée pour ajouter une nouvelle ressource.
+    ///
+    /// `@RequestBody` précise que pour récupérer la variable `Client client`, il faut prendre depuis le body de la requête.
+    @RequestMapping(value="/client", method=RequestMethod.POST)
     public Client save(@RequestBody Client client) {
         return repository.save(client);
     }
 
-    @RequestMapping(value="/client/{id}", method=RequestMethod.PUT)
+    /// **Method PUT**
+    ///
+    /// Méthode spécialisée pour mettre à jour une ressource.
+    @RequestMapping(value="/client", method=RequestMethod.PUT)
     public Client update(@RequestBody Client client) {
         return repository.save(client);
     }
 
+    /// **Method DELETE**
+    ///
+    /// Méthode spécialisée pour supprimer une ressource. Ne renvoi rien, sauf le code **200 OK**.
     @RequestMapping(value="/client/{id}", method=RequestMethod.DELETE)
     public void delete(@PathVariable long id) {
         repository.deleteById(id);
