38d45edf227f563bbc305d5737c293c20d00e748
corentin.cott@gmail.com <corentin.cott@gmail.com>
2025-09-26 12:31:31 +0200

    Ajout du model produit

---
+++ src/main/java/org/formation/model/Produit.java
@@ -0,0 +1,48 @@
+package org.formation.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Produit {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String nom;
+    private BigDecimal prix;
+    private Integer stock;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public BigDecimal getPrix() {
+        return prix;
+    }
+
+    public void setPrix(BigDecimal prix) {
+        this.prix = prix;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+}
